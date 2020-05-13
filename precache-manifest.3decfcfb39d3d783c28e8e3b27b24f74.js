92c7ae266dd6422a558b8ab066623b7b7d7c7aeb
Natalia Bujnowicz <nataliabujnowicz1987@gmail.com>
2020-05-13 20:19:23 +0100

    Updates

---
+++ precache-manifest.3decfcfb39d3d783c28e8e3b27b24f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d08f5baf5a422254a0a846f1ab323b3",
+    "url": "/To-Do-App/static/media/background.8d08f5ba.jpg"
+  },
+  {
+    "revision": "5529420073f57fa76f56",
+    "url": "/To-Do-App/static/js/runtime~main.08825c03.js"
+  },
+  {
+    "revision": "d9c5c496947482f13fff",
+    "url": "/To-Do-App/static/js/main.d092f8c5.chunk.js"
+  },
+  {
+    "revision": "e84cceccc2841b85d993",
+    "url": "/To-Do-App/static/js/2.f4426b14.chunk.js"
+  },
+  {
+    "revision": "d9c5c496947482f13fff",
+    "url": "/To-Do-App/static/css/main.fa2aee47.chunk.css"
+  },
+  {
+    "revision": "b73cfa113e2d5803b0d52b6573d8e281",
+    "url": "/To-Do-App/index.html"
+  }
+];
